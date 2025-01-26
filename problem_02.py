30d7cbf23997b54023fc3fabbf104da044a29486
ZOHAIB7689 <bzohaib754@gmail.com>
2025-01-26 10:05:17 +0500

    problem 5 complete

--- problem_02.py
+++ problem_02.py
@@ -1,20 +1,29 @@
+# Prompt the user to enter their age
 user_age = input("Your age please")
+# Convert the entered age to an integer
 user_age = int(user_age)
 
+# Prompt the user to enter the day of the week for the movie
 day = input("Enter the day for the movie -> ").lower()
+# Determine the price of the ticket based on the user's age
 price = 12 if user_age >= 18 else 8
 
+# If the day is Wednesday, apply a discount of $2
 if day == "wednesday":
-    # price = price - 2
     price -= 2
 
-print("The price of the movie ticket ticket is $", price)
- 
-user_agree = input(" Do you want to confirm the ticket sir?").lower()
+# Display the price of the movie ticket
+print("The price of the movie ticket is $", price)
 
+# Prompt the user to confirm the ticket
+user_agree = input("Do you want to confirm the ticket sir?").lower()
+
+# Check if the user's response indicates agreement
 if user_agree in ["yes", "ok", "okay", "yeah", "yup", "y", "yea", "sure", "why not"]:
+    # If the user agrees, confirm the ticket
     print("Your ticket is confirmed, sir")
     print("Enjoy the movie!")
 else:
+    # If the user does not agree, thank them for visiting
     print("Thank you for visiting us, sir")
     print("Have a nice day!")
---
+++ problem_04.py
@@ -0,0 +1,15 @@
+# Problem Determine if a fruit is ripe overripe or unripe based on its color (e.g. Banana: green is unripe, yellow is ripe, brown is overripe)
+
+fruit = "Banana "
+color = "Yellow"
+
+if fruit is "Banana":
+    if color is "Green":
+        print("The banana is unripe")
+    elif color is "Yellow":
+        print("The banana is ripe")
+    elif color is "Brown":
+        print("The banana is overripe")
+
+# Problem Determine if a fruit is ripe overripe or unripe based on its color (e.g. Banana: green is unripe, yellow is ripe, brown is overripe)
+
